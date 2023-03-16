b8ccf97c13b55972767c466a65c39d7854c064fb
Mubashir <mubashiratique98@gmail.com>
2023-03-16 07:24:26 +0500

    conflit changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import 'package:tiqarte/view/SplashScreen.dart';
 =======
 >>>>>>> ef56938... first commit.
+=======
+import 'package:tiqarte/view/SplashScreen.dart';
+>>>>>>> 1c5548d... initial
 
 void main() {
   runApp(const MyApp());
@@ -16,24 +20,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 <<<<<<< HEAD
+<<<<<<< HEAD
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: SplashScreen(),
 =======
       title: 'Flutter Demo',
+=======
+>>>>>>> 1c5548d... initial
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
+<<<<<<< HEAD
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -121,6 +120,9 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
 >>>>>>> ef56938... first commit.
+=======
+      home: SplashScreen(),
+>>>>>>> 1c5548d... initial
     );
   }
 }
